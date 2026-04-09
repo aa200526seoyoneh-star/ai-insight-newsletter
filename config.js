2ee65d8282072e8784a2747e4d7c370d5bd30e85
aa200526seoyoneh-star <aa200526seoyoneh@gmail.com>
2026-04-09 18:48:17 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 /**
  * THE AI INSIGHT - 설정 파일
- * ⚠️ 이 파일은 GitHub에 올리지 마세요! (.gitignore에 포함됨)
- * 로컬 또는 배포 서버에서만 사용합니다.
  */
 window.ADMIN_CONFIG = {
   SHEET_ID: '1XpalUZ3ap_97U4VPwCPD5603o107WAS9-B6ebxSPtFc',
   FORM_ID:  '1a2KI-p3c55Oy9xMJ9seQn_FKQ8Alx3gGRvuBjtleZ9U',
-  SUB_FORM_ACTION: 'https://docs.google.com/forms/d/e/1GSfXbBWkE4f5eqqnW1LRfwj6V9snexJTSRbUR2NwOyo/formResponse'
+  SUB_FORM_ACTION: 'https://docs.google.com/forms/d/e/1GSfXbBWkE4f5eqqnW1LRfwj6V9snexJTSRbUR2NwOyo/formResponse',
+  WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbxX56OBdahXeBP3wACO5AqzlhmPA5m74lYTYtgTZYCsd8EWJk6R6Wla_q7Lpf9A4LkVaA/exec'
 };
