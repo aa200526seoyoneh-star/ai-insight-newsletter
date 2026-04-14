fa641a4e791dc9276bf3d974ce68d6a0c82b2d78
aa200526seoyoneh-star <aa200526seoyoneh@gmail.com>
2026-04-14 14:51:34 +0900

    Add proxy URL and admin password hash to config

--- config.js
+++ config.js
@@ -3,7 +3,10 @@
  */
 window.ADMIN_CONFIG = {
   SHEET_ID: '1XpalUZ3ap_97U4VPwCPD5603o107WAS9-B6ebxSPtFc',
+  PUBLIC_SHEET_ID: '1bPc6XBR6qo7eGFUybuSNp8-fmeIFWT55PJdnE4LQ_QM',
   FORM_ID:  '1a2KI-p3c55Oy9xMJ9seQn_FKQ8Alx3gGRvuBjtleZ9U',
   SUB_FORM_ACTION: 'https://docs.google.com/forms/d/e/1GSfXbBWkE4f5eqqnW1LRfwj6V9snexJTSRbUR2NwOyo/formResponse',
-  WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbxX56OBdahXeBP3wACO5AqzlhmPA5m74lYTYtgTZYCsd8EWJk6R6Wla_q7Lpf9A4LkVaA/exec'
+  WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbxX56OBdahXeBP3wACO5AqzlhmPA5m74lYTYtgTZYCsd8EWJk6R6Wla_q7Lpf9A4LkVaA/exec',
+  PROXY_URL: '',
+  ADMIN_PW_HASH: 'a7fb82f3d2903e567fa57862ec3bef68c3a61db0b1580f5d216d2e72431911e6'
 };
